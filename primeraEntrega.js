ae98d8be071b25957d0390a08e84ab98f6df9b94
gabriel carranza <gabrielraul16@gmail.com>
2022-10-17 00:14:29 -0300

    rama3 1.01

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -1,3 +1,11 @@
+alert("Bienvenido al sistema de Compra mi nombre es Gabriel y el tuyo")
+
+let nombreComprador = prompt("ingresa tu nombre")
+
+console.log(`Hola ${nombreComprador.toLowerCase()}`)
+
+
+
 function elegirProductos() { // funcio que le pide al usuario que ingrese los productos de una lista para que se agregen a la lista
 
 
@@ -39,7 +47,7 @@ function elegirProductos() { // funcio que le pide al usuario que ingrese los pr
 
 
 
-        producto = prompt(`desea agregar otro producto a la lista \nCocacola \nPepsi \nPollo \Carne \nArroz \nHelado ademas de su ultimo producto que fue ${producto} En caso de no querer seguir comprando escribir ESC `);
+        producto = prompt(`desea agregar otro producto a la lista \nCocacola \nPepsi \nPollo \Carne \nArroz \nHelado \nademas de su ultimo producto que fue ${producto} En caso de no querer seguir comprando escribir ESC `);
 
     }
 
@@ -67,45 +75,83 @@ function aplicarIva() { //funcion que si la compra supera el valor a 1000 aplica
     } else {
         console.log(`el valor es de $${valor} no se le aplicara IVA`)
     }
-    
+
 }
 
 
 
 
 
-
+let valorConTarjeta = 0;
 
 function formaDePagoTarjeta() {
-    
+
     let pagoTarjeta = prompt("desea pagar con tarjeta")
+
     if (pagoTarjeta == `si`) {
-        console.log("en cuantas cuotas desea pagar")
+        console.log("en cuantas cuotas desea pagar hasta un maximo de 12")
+        cuota()
         for (let i = 1; i <= 12; i++) {
             if (valor < 1000) {
                 console.log(`el recargo en ${i} cuota es de %${i+3}`)
-                let valorConTarjeta = valor * (i + 3);
+
+                valorConTarjeta = valor * (i + 3);
                 valorConTarjeta = valorConTarjeta / 100;
                 valorConTarjeta = valorConTarjeta + valor;
-                console.log(valorConTarjeta);
+                console.log(valorConTarjeta.toFixed(1));
+
                 // console.log(valor)
-            } else if (valor > 1000){
+            } else if (valor > 1000) {
                 console.log(`el recargo en ${i} cuota es de %${i+3}`)
-                let valorConTarjeta = valorIva * (i + 3);
+                valorConTarjeta = valorIva * (i + 3);
                 valorConTarjeta = valorConTarjeta / 100;
                 valorConTarjeta = valorConTarjeta + valorIva;
-                console.log(valorConTarjeta);
+                console.log(valorConTarjeta.toFixed(1));
+            } else {
+                console.log("error")
             }
-            else{console.log("error")}
         }
+
     } else {
-        console.log(`el valor en efectivo es de ${valor}`)
+        if (valor < 1000) {
+
+            console.log(`el valor en efectivo es de ${valor}`)
+        } else {
+            console.log(`el valor es de ${valorIva}`)
+        }
     }
 
 }
 
 
+
 elegirProductos();
-aplicarIva()
-formaDePagoTarjeta()
+aplicarIva();
+formaDePagoTarjeta();
+
+
+// ! prueba de eleccion de cuotas //////////////////////////
+
+function cuota() {
+
+
+    let cuotasApagar = parseInt(prompt("Elegir el numero de cuotas a pagar "));
+    let cuotaCuenta = (cuotasApagar + 1) - 1
+    for (let i = cuotasApagar; i <= cuotaCuenta; i++) {
+
+        if (valor < 1000) {
+            valorConTarjeta = (valor * (i + 3)) / 100
+            valorConTarjeta = valorConTarjeta + valor
+            console.log(`Tu Total a pagar sera de ${valorConTarjeta}`)
+        } else if (valor > 1000) {
+
+            valorConTarjeta = (valorIva * (i + 3)) / 100
+            valorConTarjeta = valorConTarjeta + valorIva
+            console.log(`Tu Total a pagar sera de ${valorConTarjeta}`)
+
+        } else {alert("error")}
+    }
+}
+
+/////////////////////////////////////////////////////////
 
