c089b8ab64361e201d0c174f324b0148b3c1d06e
gabriel carranza <gabrielraul16@gmail.com>
2022-10-30 17:33:27 -0300

    trabajo 2 terminando detalles2

--- primeraEntrega.js
+++ primeraEntrega.js
@@ -1,157 +1,12 @@
 //! codigo reutilizable
-// alert("Bienvenido al sistema de Compra mi nombre es Gabriel y el tuyo")
+alert("Bienvenido al sistema de Compra mi nombre es Gabriel y el tuyo")
 
-// let nombreComprador = prompt("ingresa tu nombre")
+let nombreComprador = prompt("ingresa tu nombre")
 
-// console.log(`Hola ${nombreComprador.toLowerCase()}`)
+console.log(`Hola ${nombreComprador.toLowerCase()}`)
 
 //!
 
-// function elegirProductos() { // funcio que le pide al usuario que ingrese los productos de una lista para que se agregen a la lista
-
-
-//     valor = 0;
-//     let producto = prompt("Ingrese un producto a comprar \nCocacola \nPepsi \nCarne \nPollo \nArroz \nHelado");
-//     while (producto.toLocaleLowerCase() != `esc`) {
-
-//         switch (producto.toLowerCase()) {
-//             case "cocacola":
-//                 console.log("el valor de la Cocacola es de $320");
-//                 valor = valor + 320;
-//                 break;
-//             case "pepsi":
-//                 console.log("el valor de la Pepsi es de $220");
-//                 valor = valor + 220;
-//                 break;
-//             case "pollo":
-//                 console.log("el valor de la pollo es de $620");
-//                 valor = valor + 620;
-//                 break;
-//             case "carne":
-//                 console.log("el valor de la carne es de $950");
-//                 valor = valor + 950;
-//                 break;
-//             case "arroz":
-//                 console.log("el valor de la arroz es de $120");
-//                 valor = valor + 120;
-//                 break;
-//             case "helado":
-//                 console.log("el valor de la helado es de $1220");
-//                 valor = valor + 1220;
-//                 break;
-
-//             default:
-//                 alert(`ingreso un caracter no valido`);
-
-//         }
-
-
-
-
-//         producto = prompt(`desea agregar otro producto a la lista \nCocacola \nPepsi \nPollo \nCarne \nArroz \nHelado \nademas de su ultimo producto que fue ${producto} En caso de no querer seguir comprando escribir ESC `);
-
-//     }
-
-
-// }
-
-
-
-
-// let valor = 0;
-// let valorIva = 0;
-
-
-// function aplicarIva() { //funcion que si la compra supera el valor a 1000 aplicara el iva
-
-
-
-//     console.log(`el valor total de la lista de los productos es de $${valor}`)
-
-//     if (valor > 1000) {
-//         valorIva = valor * 1.21
-
-//         console.log(` como la compra supera los $1000 se aplica el iva \n el valor con iva es de ${valorIva.toFixed(1)} `)
-
-//     } else {
-//         console.log(`el valor es de $${valor} no se le aplicara IVA`)
-//     }
-
-// }
-
-// let valorConTarjeta = 0;
-
-// function formaDePagoTarjeta() {
-
-
-//     let pagoTarjeta = prompt("desea pagar con tarjeta")
-
-//     if (pagoTarjeta == `si`) {
-//         console.log("en cuantas cuotas desea pagar hasta un maximo de 12")
-//         cuota()
-//         for (let i = 1; i <= 12; i++) {
-//             if (valor < 1000) {
-//                 console.log(`el recargo en ${i} cuota es de %${i+3}`)
-
-//                 valorConTarjeta = valor * (i + 3);
-//                 valorConTarjeta = valorConTarjeta / 100;
-//                 valorConTarjeta = valorConTarjeta + valor;
-//                 console.log(valorConTarjeta.toFixed(1));
-
-//                 // console.log(valor)
-//             } else if (valor > 1000) {
-//                 console.log(`el recargo en ${i} cuota es de %${i+3}`)
-//                 valorConTarjeta = valorIva * (i + 3);
-//                 valorConTarjeta = valorConTarjeta / 100;
-//                 valorConTarjeta = valorConTarjeta + valorIva;
-//                 console.log(valorConTarjeta.toFixed(1));
-//             } else {
-//                 console.log("error")
-//             }
-//         }
-
-//     } else {
-//         if (valor < 1000) {
-
-//             console.log(`el valor en efectivo es de ${valor}`)
-//         } else {
-//             console.log(`el valor es de ${valorIva}`)
-//         }
-//     }
-
-// }
-
-
-
-// elegirProductos();
-// aplicarIva();
-// formaDePagoTarjeta();
-
-
-// // ! prueba de eleccion de cuotas //////////////////////////
-
-// function cuota() {
-
-
-//     let cuotasApagar = parseInt(prompt("Elegir el numero de cuotas a pagar "));
-//     let cuotaCuenta = (cuotasApagar + 1) - 1
-//     for (let i = cuotasApagar; i <= cuotaCuenta; i++) {
-
-//         if (valor < 1000) {
-//             valorConTarjeta = (valor * (i + 3)) / 100
-//             valorConTarjeta = valorConTarjeta + valor
-//             console.log(`Tu Total a pagar sera de ${valorConTarjeta}`)
-//         } else if (valor > 1000) {
-
-//             valorConTarjeta = (valorIva * (i + 3)) / 100
-//             valorConTarjeta = valorConTarjeta + valorIva
-//             console.log(`Tu Total a pagar sera de ${valorConTarjeta}`)
-
-//         } else {alert("error")}
-//     }
-// }
-
-// /////////////////////////////////////////////////////////
 
 const ArrayGaseosas = [];
 const ArrayGalletas = [];
@@ -254,11 +109,11 @@ const productoVendido = ArrayCarrito.filter((pro) => pro.vendido == true)
 
 console.log(productoVendido)
 
-
+//forEach para que el producto aplique un iva y luego acumule cantidad si se desea
 
 productoVendido.forEach((num) => {
     num.precio = (num.precio * num.cantidad ) * 1.21
-    console.log(`el producto ${num.nombre} tiene un valor de ${num.precio} `)
+    console.log(`el producto ${num.nombre} tiene un valor de ${num.precio.toFixed()} `)
 })
 
 
@@ -268,7 +123,7 @@ productoVendido.forEach((num) => {
 let resultado = productoVendido.reduce((total, producto) => total + producto.precio, 0); //0 es el inicio
 
 
-console.log(`su total a pagar es de ${resultado}`);
+console.log(`su total a pagar es de ${resultado.toFixed(1)}`);
 
 
 
@@ -318,7 +173,7 @@ function cuota() {
         if (resultado) {
             valorConTarjeta = (resultado * (i + 3)) / 100
             valorConTarjeta = valorConTarjeta + resultado
-            console.log(`Tu Total a pagar sera de ${valorConTarjeta}`)
+            console.log(`Tu Total a pagar sera de ${valorConTarjeta.toFixed(1)}`)
         } else {
             alert("error")
         }
